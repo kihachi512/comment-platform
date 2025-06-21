0c306a8654fe4ea139fd661b7c7a01fef4aa8a91
kihachi512 <nerima.konbu2@gmail.com>
2025-06-21 20:49:10 +0900

    Update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
 // pages/_app.tsx
+import { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps: { session, ...pageProps } }) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider session={session}>
+    <SessionProvider session={(pageProps as any).session}>
       <Component {...pageProps} />
     </SessionProvider>
   );
