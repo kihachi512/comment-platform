ab20a371dddfe513a3cd81bb719a3818432b5d01
kihachi512 <63526867+kihachi512@users.noreply.github.com>
2025-07-23 15:48:27 +0900

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import styles from "../styles/Footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      {new Date().getFullYear()} ©桃缶之間
+      ©{new Date().getFullYear()}&nbsp; 桃缶之間
     </footer>
   );
 }
